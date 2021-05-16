284a8e91f87a6a3bccaff09ed74ac7e9310dd17e
prahladinala <prahlad2018@gmail.com>
2021-05-16 23:00:13 +0530

    Rendering HTML

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const http = require('http')
 // http.createServer((req, res) => { }).listen()
 
 const server = http.createServer((req, res) => {
-    console.log(123)
+    // console.log(123)
+    res.statusCode = 200
+    res.setHeader("Content-Type", 'text/html')
+    res.write('<h1>Hello World</h1>')
+    res.end()
 })
 
 const PORT = process.env.PORT || 5000
